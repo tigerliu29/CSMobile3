f77f2df27dd28e68e61cdd49e33b2619cc40c03a
刘勇泰 <liuyongtai@acm.org>
2018-04-08 15:27:04 +0800

    外观调整

---
+++ src/pages/new-post/new-post.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the NewPostPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-new-post',
+  templateUrl: 'new-post.html',
+})
+export class NewPostPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NewPostPage');
+  }
+
+}
